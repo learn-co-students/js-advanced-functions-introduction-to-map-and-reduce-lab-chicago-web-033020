ce7dacbda2587e3dc7628411b2419e717cdcb075
Shau Yin Luk <ivan.luk@icloud.com>
2020-05-18 12:34:49 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,58 @@
 // Your code here
+function mapToNegativize(array){
+  let newA = []
+  for (let i = 0; i < array.length; i++){
+    newA.push(array[i] * -1) 
+  }
+  return newA
+}
+
+function mapToNoChange(array){
+  let newA = []
+  for (let i = 0; i < array.length; i++){
+    newA.push(array[i]) 
+  }
+  return newA
+}
+
+function mapToDouble(array){
+  let newA = []
+  for (let i = 0; i < array.length; i++){
+    newA.push(array[i] * 2) 
+  }
+  return newA
+}
+
+function mapToSquare(array){
+  let newA = []
+  for (let i = 0; i < array.length; i++){
+    newA.push(array[i] ** 2) 
+  }
+  return newA
+}
+
+function reduceToTotal(source, startTot=0){
+  let tot = startTot
+  for (let i = 0; i < source.length; i++){
+    tot = tot + source[i] 
+  }
+  return tot
+}
+
+function reduceToAllTrue(source){
+  for (let i = 0; i < source.length; i++){
+    if (!source[i]){
+      return false
+    }
+  }
+  return true
+}
+function reduceToAnyTrue(source){
+ 
+  for (let i = 0; i < source.length; i++){
+    if (source[i]){
+      return true
+    }
+  }
+  return false
+}
